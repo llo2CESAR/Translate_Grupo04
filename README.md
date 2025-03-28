74e4e57c92c39e1ec78f2474d8bf667847e314e1
llo2CESAR <llo2@cesar.org.br>
2025-03-28 12:33:44 -0400

    Arquivo README.md atualizado

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Translate_Grupo04
-Criar um sistema de tradução usando API externa do Google
+# Visão Geral 
+O sistema de tradução permite que os usuários insiram um texto em um idioma e escolham o idioma de destino para a tradução. A API do Google é utilizada para realizar a tradução de forma rápida e precisa.
+
+# Tecnologias Utilizadas
+ * Linguagem de Programação: Python
+ * Framework: Flash (para criar API)
+ * API: Google Cloud Translation API
+ Geranciador de Pacotes: pip
+
+ # Pré-requisitos
+
+Antes de começar, é necessário ter o seguinte instalado na máquina:
+* Python 3.x
+* pip (gerenciador de pacotes Python)
+* Conta do Google e acesso à Google Cloud Translation API
+
+# Instalação
+
+1. Clone este repositório:
+   * git clone https://github.com/llo2CESAR/Translate_Grupo04.git
+   * cd C:\Users\seu_usuario\sua_pasta>Translate_Grupo0
+
---
+++ Voice_Translate.py
@@ -0,0 +1,76 @@
+# importar as bibliotecas
+
+from translate import Translator 
+import pyttsx3 
+
+
+
+#iniciar o pyttsx3
+
+robo = pyttsx3.init()
+
+voices = robo.getProperty('voices')
+
+robo.setProperty('voice',voices[1].id)
+
+
+
+#criar a nossa lista de idiomas
+
+idiomas = ['pt-br', 'en-us', 'de']
+
+
+
+#criar um menu com os idiomas suportados
+
+print('Lista de idiomas suportados: ')
+
+print('-='* 30)
+
+print('''1-Português 2-Inglês 3-Alemão''')
+
+print('-=' * 30)
+
+
+
+#Escolher o idioma a ser traduzido
+
+idioma_1 = int(input('Digite o número correspondente ao idioma a ser traduzido: '))-1
+
+print('-='*30)
+
+while idioma_1 not in [1, 2, 3]:
+
+    print('Digite um valor válido')
+
+    print('-=' *30)
+
+    idiona_1 = int(input('Digite o número correspondente ao idioma a ser traduzido: '))-1
+
+idioma_2 = int(input('Digite o número correspondente ao idioma a ser traduzido: '))-1
+
+print('-='*30)
+
+
+
+#criar o tradutor
+
+
+
+tradutor = Translator(from_lang=idiomas[idioma_1],to_lang=idiomas[idioma_2])
+
+
+
+#pegar a frase e traduzir ela
+
+frase = tradutor.translate(input('Digite a frase a ser traduzida: '))
+
+
+
+robo.say(frase)
+
+print(frase)
+
+robo.runAndWait()
+
+robo.stop()
